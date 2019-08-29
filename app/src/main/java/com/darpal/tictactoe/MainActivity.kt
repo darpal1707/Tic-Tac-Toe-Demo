a189ae0f03e7772614c7bc06f478920d4c1732ed
darpal1995 <darpaldhyani2014@gmail.com>
2019-08-29 13:30:28 -0700

    scorecard update

--- app/src/main/java/com/darpal/tictactoe/MainActivity.kt
+++ app/src/main/java/com/darpal/tictactoe/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.darpal.tictactoe
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -16,133 +17,203 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClick(view: View) {
-        val buttonSelected = view as Button
-        var cellID = 0
-        when(buttonSelected.id) {
-            R.id.btn1_row1 -> cellID = 1
-            R.id.btn2_row1 -> cellID = 2
-            R.id.btn3_row1 -> cellID = 3
-            R.id.btn1_row2 -> cellID = 4
-            R.id.btn2_row2 -> cellID = 5
-            R.id.btn3_row2 -> cellID = 6
-            R.id.btn1_row3 -> cellID = 7
-            R.id.btn2_row3 -> cellID = 8
-            R.id.btn3_row3 -> cellID = 9
+    fun buClick( view:View){
+
+        val buSelected = view as Button
+
+        var cellId = 0
+        when(buSelected.id){
+            R.id.bu1 -> cellId = 1
+            R.id.bu2 -> cellId = 2
+            R.id.bu3 -> cellId = 3
+            R.id.bu4 -> cellId = 4
+            R.id.bu5 -> cellId = 5
+            R.id.bu6 -> cellId = 6
+            R.id.bu7 -> cellId = 7
+            R.id.bu8 -> cellId = 8
+            R.id.bu9 -> cellId = 9
         }
-        playGame(cellID,buttonSelected)
+
+
+
+
+        playGame(cellId,buSelected)
+
     }
+
 
     var activePlayer = 1
+
     var player1 = ArrayList<Int>()
     var player2 = ArrayList<Int>()
-    fun playGame(cellID:Int, buttonSelected:Button){
-        if (activePlayer == 1){
-            buttonSelected.text = "X"
-            buttonSelected.setBackgroundColor(Color.parseColor("#1DE9B6"))
-            player1.add(cellID)
+
+    fun playGame(cellId:Int, buSelected:Button){
+
+
+        if( activePlayer == 1 ){
+            buSelected.text = "X"
+            buSelected.setBackgroundColor(Color.parseColor("#1DE9B6"))
+            player1.add(cellId)
             activePlayer = 2
             autoPlay()
-        }
-        else{
-            buttonSelected.text = "O"
-            buttonSelected.setBackgroundColor(Color.parseColor("#64FFDA"))
-            player2.add(cellID)
+
+        }else{
+
+            buSelected.text = "O"
+            buSelected.setBackgroundColor(Color.parseColor("#00BFA5"))
+            player2.add(cellId)
             activePlayer = 1
+
         }
-        buttonSelected.isEnabled = false
-        checkForWinner()
+
+        buSelected.isEnabled = false
+
+        checkWinner()
     }
 
-    fun checkForWinner(){
-        var winner = -1
+
+    fun checkWinner() {
+
+        var winer = -1
+
 
         // row 1
         if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
-            winner = 1
+            winer = 1
         }
         if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
-            winner = 2
+            winer = 2
         }
+
+
         // row 2
         if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
-            winner = 1
+            winer = 1
         }
         if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
-            winner = 2
+            winer = 2
         }
+
         // row 3
         if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
-            winner = 1
+            winer = 1
         }
         if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
-            winner = 2
+            winer = 2
         }
+
+
         // col 1
         if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
-            winner = 1
+            winer = 1
         }
         if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
-            winner = 2
+            winer = 2
         }
+
+
         // col 2
         if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
-            winner = 1
+            winer = 1
         }
         if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
-            winner = 2
+            winer = 2
         }
+
+
         // col 3
         if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
-            winner = 1
+            winer = 1
         }
         if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
-            winner = 2
+            winer = 2
         }
 
 
-        if (winner == 1) {
-            //player1WinsCounts += 1
+
+        if (winer == 1) {
+            player1WinsCounts += 1
             Toast.makeText(this, "Player 1 win the game", Toast.LENGTH_LONG).show()
-            //restartGame()
+            restartGame()
 
-        } else if (winner == 2) {
-            //player2WinsCounts += 1
+        } else if (winer == 2) {
+            player2WinsCounts += 1
             Toast.makeText(this, "Player 2 win the game", Toast.LENGTH_LONG).show()
-            //restartGame()
+            restartGame()
         }
+
 
     }
 
     fun autoPlay(){
-        var checkEmptyCells = java.util.ArrayList<Int>()
-        for (cellID in 1..9){
-            if (!player1.contains(cellID) || player2.contains(cellID)){
-                checkEmptyCells.add(cellID)
+
+
+        var emptyCells = ArrayList<Int>()
+
+        for( cellId in 1..9){
+
+            if( !(player1.contains(cellId) || player2.contains(cellId))){
+                emptyCells.add(cellId)
             }
         }
 
-        var r = Random
-        val randIndex = r.nextInt(checkEmptyCells.size)
-        val cellID = checkEmptyCells[randIndex]
-        var btnSelected : Button?
-        btnSelected = when(cellID){
-            1 -> btn1_row1
-            2 -> btn2_row1
-            3 -> btn3_row1
-            4 -> btn1_row2
-            5 -> btn2_row2
-            6 -> btn3_row2
-            7 -> btn1_row3
-            8 -> btn2_row3
-            9 -> btn3_row3
+        if(emptyCells.size==0){
+            restartGame()
+        }
+        val r = java.util.Random()
+        val randIndex = r.nextInt(emptyCells.size )
+        val cellId = emptyCells[randIndex]
 
-            else ->{
-                btn1_row1
-            }
+        var buSelected:Button?
+        buSelected =  when(cellId){
+            1-> bu1
+            2-> bu2
+            3-> bu3
+            4-> bu4
+            5-> bu5
+            6-> bu6
+            7-> bu7
+            8-> bu8
+            9-> bu9
+            else ->{ bu1}
+
         }
 
-        playGame(cellID, btnSelected)
+        playGame(cellId, buSelected)
+
     }
 
+    var player1WinsCounts = 0
+    var player2WinsCounts = 0
+
+    fun restartGame(){
+
+        activePlayer = 1
+        player1.clear()
+        player2.clear()
+
+        for(cellId in 1..9){
+
+            var buSelected:Button? = when(cellId){
+                1-> bu1
+                2-> bu2
+                3-> bu3
+                4-> bu4
+                5-> bu5
+                6-> bu6
+                7-> bu7
+                8-> bu8
+                9-> bu9
+                else ->{ bu1}
+
+            }
+            buSelected!!.text = ""
+            buSelected!!.setBackgroundColor(Color.WHITE)
+            buSelected!!.isEnabled = true
+        }
+
+        Toast.makeText(this,"Player1: $player1WinsCounts, Player2: $player2WinsCounts", Toast.LENGTH_LONG).show()
+
+
+    }
 }
